9229faa656028c991f1257be983f797d72a8a786
Md Mamun <juborajvai22@gmail.com>
2022-05-14 16:04:07 +0600

    update

--- index.js
+++ index.js
@@ -16,23 +16,53 @@ const uri = `mongodb+srv://myAdmin:GnbWOPxkDrjF7ohN@admin.lzlwc.mongodb.net/myFi
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-async function run(){
-      try{
+async function run() {
+      try {
             await client.connect();
             const patientCollection = client.db("patient").collection("service");
+            const bookingsCollection = client.db("patient").collection("booking");
 
             // read to mongodb 
 
-            app.get('/service' , async (req , res) =>{
+            app.get('/service', async (req, res) => {
                   const query = {}
                   const cursor = patientCollection.find(query)
                   const service = await cursor.toArray()
                   res.send(service)
             })
 
+
+            // booking caletion 
+            // booking korar fole eki bokking bar bar korte partesi eti solve jonno 
+            app.post('/booking', async (req, res) => {
+                  
+
+                  // step 1 check booking jeta add hoise
+                  const booking = req.body
+                  console.log(booking);
+
+                  const query = {
+                        treatment: booking.treatment, date: booking.date, patient: booking.patient
+                  }
+                  console.log(query);
+
+                  // step 2- find query
+                  const exists = await bookingsCollection.findOne(query)
+
+                  // step 3 - chack candition 
+                  if(exists){
+                       return res.send({success: false , booking: exists})
+                  }
+
+                  const result = await bookingsCollection.insertOne(booking)
+                  return res.send({success: true , result})
+            })
+
+           
+
       }
 
-      finally{
+      finally {
 
       }
 
@@ -56,9 +86,9 @@ run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+      res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`start dector protol ${port}`)
+      console.log(`start dector protol ${port}`)
 })
